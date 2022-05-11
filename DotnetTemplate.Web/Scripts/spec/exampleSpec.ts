3edf88939062546a356e498884307ce5de92c5d8
Mateusz <matjadach@gmail.com>
2022-05-11 16:09:53 +0100

    changes

--- DotnetTemplate.Web/Scripts/spec/exampleSpec.ts
+++ DotnetTemplate.Web/Scripts/spec/exampleSpec.ts
@@ -4,14 +4,21 @@ describe('Example', () => {
   it('can run functionOne', () => {
     // Given
     const one = 1;
-    const two = 2;
 
     // When
     functionOne();
-    functionTwo();
 
     // Then
     expect(1).toEqual(one);
+  });
+  it('can run functionTwo', () => {
+    // Given
+    const two = 2;
+
+    // When
+    functionTwo();
+
+    // Then
     expect(2).toEqual(two);
   });
 });
