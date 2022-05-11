0ccd286aa526daf83dba1faebde804f2d4f00524
Mateusz <matjadach@gmail.com>
2022-05-11 15:56:51 +0100

    code coverage

--- DotnetTemplate.Web/Scripts/spec/exampleSpec.ts
+++ DotnetTemplate.Web/Scripts/spec/exampleSpec.ts
@@ -1,14 +1,17 @@
-import { functionOne } from '../home/example';
+import { functionOne, functionTwo } from '../home/example';
 
 describe('Example', () => {
   it('can run functionOne', () => {
     // Given
     const one = 1;
+    const two = 2;
 
     // When
     functionOne();
+    functionTwo();
 
     // Then
     expect(1).toEqual(one);
+    expect(2).toEqual(two);
   });
 });
